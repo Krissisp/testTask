1112818dade651771352c24897bcaf1bcc19d7f7
Kristina Palenykh <kpalenykh@bk.ru>
2021-12-06 20:37:03 +0300

    solution through a loop with one iteration of the array

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,19 +1,33 @@
 import { expect, it } from '@jest/globals';
-import sumToMinNum from '../src/index.js';
+import sumTwoMinNum from '../src/index.js';
 
 
 it('test: empty array', () => {
-    expect(sumToMinNum([])).toBe(0)
+    expect(sumTwoMinNum([])).toBe(0)
+})
+
+it('test: singular', () => {
+    expect(sumTwoMinNum([5])).toBe(5)
+})
+
+it('test: two numbers', () => {
+    expect(sumTwoMinNum([1, 6])).toBe(7)
 })
 
 it('test', () => {
-    expect(sumToMinNum([0, -2, 28, 33, -4])).toBe(-6)
+    expect(sumTwoMinNum([0, -2, 28, 33, -4])).toBe(-6)
 })
 
 it('test: long array', () => {
-    expect(sumToMinNum([0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 3, 554, 15 ])).toBe(1)
+    expect(sumTwoMinNum([0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 3, 554, 15 ])).toBe(1)
 })
 
 it('test: repeat numbers', () => {
-    expect(sumToMinNum([-10, -10, 2, 3, -15, 16, 3, 554, 15 ])).toBe(-25)
+    expect(sumTwoMinNum([-10, -10, 2, 3, -15, 16, 3, 554, 15 ])).toBe(-25)
 })
+
+it('test: less than the maximum and more than the minimum', () => {
+    expect(sumTwoMinNum([6, 4, 5])).toBe(9)
+})
+
+
--- src/index.js
+++ src/index.js
@@ -2,6 +2,27 @@ export default function sumTwoMinNum(arrayNumbers) {
     if(arrayNumbers.length === 0) {
         return 0;
     }
-    const [numMin1, numMin2] = [...arrayNumbers].sort((a, b) => a - b);
-    return numMin1 + numMin2;
+
+    if(arrayNumbers.length >= 1 && arrayNumbers.length <= 2)  {
+        return arrayNumbers[1] ? arrayNumbers[0] + arrayNumbers[1] : arrayNumbers[0];
+    }  
+
+    let num1 = Math.min(arrayNumbers[0], arrayNumbers[1]);
+    let num2 = Math.max(arrayNumbers[0], arrayNumbers[1]);
+
+    for(let i = 2; i < arrayNumbers.length; i += 1) {
+        if(arrayNumbers[i] < num1) {
+            num2 = num1;
+            num1 = arrayNumbers[i];
+            continue;
+        }
+
+        if(arrayNumbers[i] < num2) {
+            num2 = arrayNumbers[i];
+        }
+
+    }
+
+    return num1 + num2;
+    
 }
